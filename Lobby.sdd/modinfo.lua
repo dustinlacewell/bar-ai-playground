d232be51e6d0362dec108fc48859b8c380bc4b8d
Dustin Lacewell <dlacewell@gmail.com>
2025-11-11 21:51:31 -0600

    Add lobby mod

---
+++ Lobby.sdd/modinfo.lua
@@ -0,0 +1,15 @@
+return {
+  name = 'BYAR Chobby',
+  shortName = 'BYAR_CHOBBY',
+  description = 'BYAR mutator for Chobby',
+  version = '$VERSION',
+  mutator = 'Official',
+  modtype = 5,
+  onlyLocal = true,
+  depend = {
+      -- For developing base chobby, switch out dependency
+      'rapid://byar-chobby:test', --this uses rapid pinned chobby
+      --'Chobby $VERSION', -- this specifies chobby.sdd working path
+  },
+
+}
